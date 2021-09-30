0723f1f32f2d0b584f66587bc03e404103736c19
elsajonsson <36952699+elsajonsson@users.noreply.github.com>
2021-09-30 11:15:07 +0200

    removed unneccesary commands

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,5 +1,23 @@
 package cli
 
 import "fmt"
+import "strings"
 
+func Cli() {
+	for {
+		var input string
+		fmt.Printf("Command: \n")
+		fmt.Scanln(&input)
+
+		switch {
+		case strings.Contains(input,"PUT"):
+			fmt.Println("put command")
+		case strings.Contains(input, "GET"):
+			fmt.Println("get command")
+		case strings.Contains(input, "PING"):
+			fmt.Println("ping command")
+		case input == "EXIT":
+			fmt.Println("exit command")
+		}
+	}
 }
