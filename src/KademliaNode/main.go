942efe0c44f4d06f6b4dc29e43cb7721146007f0
elsajonsson <36952699+elsajonsson@users.noreply.github.com>
2021-10-01 11:41:59 +0200

    CLI concurrency
    All nodes have CLI and can listen to the network at the same time

--- src/KademliaNode/main.go
+++ src/KademliaNode/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/albernnV/D7024E/pkg/kademlia"
+import (
+	"github.com/albernnV/D7024E/pkg/cli"
+	"github.com/albernnV/D7024E/pkg/kademlia"
+)
 
 func main() {
+	go cli.Cli()
 	net := &kademlia.Network{}
 	//kademlia.Listen("", 8000)
 	net.SendPingMessage()
