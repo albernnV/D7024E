cad0b0470464020a98c492d9b4ffb27f81771b7e
elsajonsson <36952699+elsajonsson@users.noreply.github.com>
2021-10-13 13:16:50 +0200

    routing table test done

--- pkg/kademlia/routingtable_test.go
+++ pkg/kademlia/routingtable_test.go
@@ -21,27 +21,27 @@ func TestRoutingTable(t *testing.T) {
 	}
 }
 
-func TestgetBucketIndex(t *testing.T) {
+
+func TestGetBucketIndex(t *testing.T) {
 	me := NewContact(NewKademliaID("0000000000000000000000000000000000000000"), "")
 	rt := NewRoutingTable(me)
-	contactID := NewKademliaID("0000000000000000000000000000000000000001")
+	contactID := NewKademliaID("0000000000000000000000000000000000000000")
 	bucketIndex := rt.getBucketIndex(contactID)
-	correctIndex := 99999999
-	fmt.Println("index: "+ string(bucketIndex))
+	// 1 is one byte so 0001
+	correctIndex := 159
 	if !(bucketIndex == correctIndex) {
 		t.Errorf("Wrong bucket id, want: %v, got: %v", correctIndex, bucketIndex)
 	}
-
 }
 
-/*
+
 func TestAddContactRT(t *testing.T) {
 	me := NewContact(NewKademliaID("0000000000000000000000000000000000000000"), "")
 	rt := NewRoutingTable(me)
 	//should be in bucket with index 2: [2^2 - 2^3)
-	contact := NewContact(NewKademliaID("0000000000000000000000000000000000000001"), "")
+	contact := NewContact(NewKademliaID("1000000000000000000000000000000000000000"), "")
 	rt.AddContact(contact)
-	bucket := rt.buckets[0]
+	bucket := rt.buckets[3]
 	bool := true
 	for e := bucket.list.Front(); e != nil; e = e.Next() {
 		nodeID := e.Value.(Contact).ID
@@ -53,14 +53,32 @@ func TestAddContactRT(t *testing.T) {
 		t.Errorf("The contact is not in the right bucket")
 	}
 }
-*/
 
-/*
+
 func TestFindClosestContacts(t *testing.T) {
+	me := NewContact(NewKademliaID("0000000000000000000000000000000000000000"), "")
+	rt := NewRoutingTable(me)
+	contact1 := NewContact(NewKademliaID("1000000000000000000000000000000000000000"), "")
+	contact2 := NewContact(NewKademliaID("1100000000000000000000000000000000000000"), "")
+	contact3 := NewContact(NewKademliaID("1110000000000000000000000000000000000000"), "")
+	rt.AddContact(contact1)
+	rt.AddContact(contact2)
+	rt.AddContact(contact3)
+	numContacts := 2
 
+	closestLstTest := rt.FindClosestContacts(NewKademliaID("1110000000000000000000000000000000000000"), numContacts)
+	var closestLstCorrect []Contact
+	closestLstCorrect = append(closestLstCorrect, contact3)
+	closestLstCorrect = append(closestLstCorrect, contact2)
+
+	if len(closestLstTest) == numContacts {
+		for i := 0; i < numContacts; i++ {
+			if closestLstTest[i].ID != closestLstCorrect[i].ID {
+				t.Errorf("Contacts is wrong, expected: %v, got: %v", closestLstCorrect[i].ID, closestLstTest[i].ID)
+			}
+		}
+	} else {
+		t.Errorf("Number of contacts is wrong, expected: %v, got: %v", numContacts, len(closestLstTest))
+	}
 }
 
-func TestUpdateRoutingTable(t *testing.T) {
-
-}
-*/
